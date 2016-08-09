5e99930a05968f442cdce60abe6307357a8c6986
David Walsh <dawalsh@gmail.com>
2016-08-08 23:37:56 -0400

    Implement extend

--- src/underbar.js
+++ src/underbar.js
@@ -247,6 +247,13 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var result = obj;
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        result[key] = arguments[i][key];
+      }
+    }
+    return result;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
